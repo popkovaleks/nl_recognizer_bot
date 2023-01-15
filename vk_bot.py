ba76df7a1b9d12c9e93e675e5b26e1bd58dc455e
Aleksandr <popkov.aleks.v@yandex.ru>
2023-01-15 16:24:39 +0500

    add dialogflow for vk bot

--- vk_bot.py
+++ vk_bot.py
@@ -1,23 +1,27 @@
-import vk_api
+import random
+import vk_api as vk
+
 from vk_api.longpoll import VkLongPoll, VkEventType
 from environs import Env
+
+from dialogflow_intent import detect_intent
 
 
 env = Env()
 env.read_env()
 
 VK_TOKEN = env('VK_API_TOKEN')
-
-import random
-
-import vk_api as vk
-from vk_api.longpoll import VkLongPoll, VkEventType
+PROJECT_ID=env('PROJECT_ID')
+GOOGLE_APPLICATION_CREDENTIALS=env('GOOGLE_APPLICATION_CREDENTIALS')
 
 
 def echo(event, vk_api):
+    user_id = event.user_id
+    message = event.text
+    response_text = detect_intent(project_id=PROJECT_ID, session_id=user_id, texts=[message], language_code='ru')
     vk_api.messages.send(
-        user_id=event.user_id,
-        message=event.text,
+        user_id=user_id,
+        message=response_text,
         random_id=random.randint(1,1000)
     )
 
