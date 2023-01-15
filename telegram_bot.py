cddd988fca17a349b9d2cb2db7693d955a9eda05
Aleksandr <popkov.aleks.v@yandex.ru>
2023-01-15 16:16:19 +0500

    add VK bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,7 +3,8 @@ import logging
 from environs import Env
 from telegram import Update
 from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
-from google.cloud import dialogflow
+
+from dialogflow_intent import detect_intent
 
 
 env = Env()
@@ -28,32 +29,6 @@ def main():
     updater.start_polling()
     updater.idle()
 
-
-def detect_intent(project_id, session_id, texts, language_code):
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-    print("Session path: {}\n".format(session))
-
-    for text in texts:
-        text_input = dialogflow.TextInput(text=text, language_code=language_code)
-
-        query_input = dialogflow.QueryInput(text=text_input)
-
-        response = session_client.detect_intent(
-            request={"session": session, "query_input": query_input}
-        )
-
-        print("=" * 20)
-        print("Query text: {}".format(response.query_result.query_text))
-        print(
-            "Detected intent: {} (confidence: {})\n".format(
-                response.query_result.intent.display_name,
-                response.query_result.intent_detection_confidence,
-            )
-        )
-        response_text = response.query_result.fulfillment_text
-        print("Fulfillment text: {}\n".format(response_text))
-        return response_text
 
 
 def start(update: Update, context: CallbackContext):
